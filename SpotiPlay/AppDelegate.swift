fa10485df9d3825a5c63d88b2649665341f3a535
raza-qazi <rmqazi@hotmail.com>
2017-09-17 17:37:25 -0600

    Initial Commit

---
+++ SpotiPlay/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SpotiPlay
+//
+//  Created by Raza Qazi on 2017-09-17.
+//  Copyright © 2017 Raza Qazi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SpotiPlay/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpotiPlay
+//
+//  Created by Raza Qazi on 2017-09-17.
+//  Copyright © 2017 Raza Qazi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
